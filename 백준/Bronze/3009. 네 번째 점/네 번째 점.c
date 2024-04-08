0607f2cc2744666b7f63f4eeca1372fc9afa018a
roor48 <109197337+roor48@users.noreply.github.com>
2024-04-08 21:26:34 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.c
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.c
@@ -1,59 +1,14 @@
 #include <stdio.h>
 
-int arr[3][2];
-
-int directions[4] = {-9999, -9999, 9999, 9999}; // top, right, bottom, left
-int directionsCnt[4] = {0};
-
 int main()
 {
+	int arr[3][2];
 	for (int i = 0; i < 3; i++)
 	{
 		scanf("%d %d\n", &arr[i][0], &arr[i][1]);
-
-		for (int j = 0; j < 4; j++)
-		{
-			if (j < 2)
-			{
-				if (directions[j] < arr[i][j&1])
-					directions[j] = arr[i][j&1];
-			}
-			else if (directions[j] > arr[i][j&1])
-			{
-				directions[j] = arr[i][j&1];
-			}
-		}
 	}
 
-	for (int i = 0; i < 3; i++)
-	{
-		if (directions[0] == arr[i][0])
-			directionsCnt[0]++;
-		else if (directions[2] == arr[i][0])
-			directionsCnt[2]++;
-
-		if (directions[1] == arr[i][1])
-			directionsCnt[1]++;
-		else if (directions[3] == arr[i][1])
-			directionsCnt[3]++;
-	}
-
-	int y, x;
-	for (int i = 0; i < 4; i++)
-	{
-		if (i&1)
-		{
-			if (directionsCnt[i] == 1)
-				x = i;
-		}
-		else
-		{
-			if (directionsCnt[i] == 1)
-				y = i;
-		}
-	}
-
-	printf("%d %d", directions[y], directions[x]);
+	printf("%d %d", arr[0][0]^arr[1][0]^arr[2][0], arr[0][1]^arr[1][1]^arr[2][1]);
 
 	return 0;
 }
