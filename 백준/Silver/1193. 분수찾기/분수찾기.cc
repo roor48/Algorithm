c4cab4ccec7f4cc40e8bcbf1c894d02ff57507a2
roor48 <109197337+roor48@users.noreply.github.com>
2025-04-30 01:40:48 +0900

    [Silver V] Title: 분수찾기, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+
+
+    int L = 1;
+    int y=1, x=1;
+    int dirY=0, dirX=1;
+    for (int i = 2; i <= X; i++)
+    {
+        x += dirX;
+        y += dirY;
+
+        if (x<=0)
+            x = 1;
+        if (y<=0)
+            y = 1;
+
+        if (x > L && y==1) // 우측 -> 좌측
+        {
+            dirX = -1;
+            dirY = 1;
+
+            L++;
+        }
+
+        if (y > L && x==1) // 좌측 -> 우측
+        {
+            dirX = 1;
+            dirY = -1;
+
+            L++;
+        }
+    }
+
+    cout << y << '/' << x << "\n";
+    return 0;
+}
