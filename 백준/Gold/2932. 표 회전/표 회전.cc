bb8a433719a7829aae82fcdaa059fc1412c862be
roor48 <109197337+roor48@users.noreply.github.com>
2025-05-14 15:55:06 +0900

    [Gold IV] Title: 표 회전, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/2932. 표 회전/표 회전.cc
+++ 백준/Gold/2932. 표 회전/표 회전.cc
@@ -20,7 +20,7 @@ int dist(int a)
 }
 
 
-d X[10000];
+d X[1000];
 int main()
 {
     cin >> N >> K;
@@ -48,7 +48,7 @@ int main()
             if (X[i].cur_pos.second == X[j].cur_pos.second) // 같은 행에 있으면 열 회전
                 X[j].cur_pos.first = dist(X[j].cur_pos.first + distY);
         }
-            
+
         cout << distY + distX << '\n';
     }
 
@@ -57,4 +57,3 @@ int main()
 
 // X의 위치가 C열이 될 때 까지, X가 있는 행을 회전시킨다.
 // X의 위치가 R행이 될 때 까지, X가 있는 열을 회전시킨다.
-
