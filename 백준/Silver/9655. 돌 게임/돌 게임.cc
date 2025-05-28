34b43628929fc7d18f98e68321a84845b0b82fb4
roor48 <109197337+roor48@users.noreply.github.com>
2025-05-28 15:46:47 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,30 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int dp[1001];
-
-int f(int n)
-{
-    if (n<0 || n>1000) return 0;
-    if (dp[n]) return dp[n];
-
-    if (f(n-1)==1)
-        return dp[n] = 2;
-    if (f(n-3)==1)
-        return dp[n] = 2;
-    return dp[n] = 1;
-}
-
 int main()
 {
     int n;
     cin >> n;
 
-    dp[0] = 2;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 1;
-
-    cout << (f(n)==1 ? "SK" : "CY");
+    cout << (n&1 ? "SK" : "CY");
     return 0;
 }
