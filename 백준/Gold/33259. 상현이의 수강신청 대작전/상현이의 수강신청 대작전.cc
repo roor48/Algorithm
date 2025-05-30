c5f8af9d0ae7f8e4adecdcc21ef373b3e1251990
roor48 <109197337+roor48@users.noreply.github.com>
2025-05-30 15:17:27 +0900

    [Gold IV] Title: 상현이의 수강신청 대작전, Time: 108 ms, Memory: 99756 KB -BaekjoonHub

---
+++ 백준/Gold/33259. 상현이의 수강신청 대작전/상현이의 수강신청 대작전.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// C의 합이 M을 넘지 않으면서 P의 합이 가장 크게 
+int N, M;
+int P[5001], C[5001];
+
+int dp[5001][5001]; // i번 물건 볼때 j 학점을 얻는 모든 경우의 가치 중 최대치
+
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> P[i] >> C[i];
+    }
+
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j <= M; j++)
+        {
+            if (j < C[i])
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+            else
+            {
+                if (dp[i-1][j] >= dp[i-1][j-C[i]] + P[i])
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j-C[i]] + P[i];
+                }
+            }
+        }
+    }
+
+
+    int maxVal = -1, lastNum = -1, lastCredit = -1;
+    for (int i = 0; i <= N; i++)
+    {
+        for (int j = 0; j <= M; j++)
+        {
+            if (maxVal < dp[i][j])
+            {
+                maxVal = dp[i][j];
+                lastNum = i;
+                lastCredit = j;
+            }
+        }
+    }
+
+    if (lastNum==0 && lastCredit==0)
+    {
+        cout << "-1";
+        return 0;
+    }
+    vector<int> ans;
+    for (int num = lastNum; num > 0; num--)
+    {
+        if (dp[num][lastCredit] == dp[num-1][lastCredit]) // num번 강의를 안 들었으면
+            continue;
+        
+        lastCredit -= C[num];
+        ans.push_back(num);
+    }
+
+    cout << ans.size();
+    for (int i = ans.size()-1; i>=0; i--)
+        cout << ' ' << ans[i];
+
+    return 0;
+}
