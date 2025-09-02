972ccdf8d09dabdb95dd78bf289f0c215b10aa7d
roor48 <109197337+roor48@users.noreply.github.com>
2025-09-03 03:18:33 +0900

    [Gold V] Title: 물병, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n, k;
+    cin >> n >> k;
+
+    if (__builtin_popcount(n) <= k) {
+        cout << "0";
+        return 0;
+    }
+
+    int cnt = 0, h;
+    for (int i = 31; i >= 0; i--) {
+        if (n&(1<<i))
+        {
+            n &= ~(1<<i);
+            cnt++;
+        }
+        
+        if (k <= cnt) {
+            cout << (n ^ ((1<<i)-1))+1;
+
+            return 0;
+        }
+    }
+    
+    cout << "-1";
+    return 0;
+}
