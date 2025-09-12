5f7333c33d8cf91ad8fe1c3e4742c631a9e8e115
roor48 <109197337+roor48@users.noreply.github.com>
2025-09-12 17:33:44 +0900

    [Bronze II] Title: 분산처리, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -1,21 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> v;
 int main() {
     int K, a, b;
     cin >> K;
-    int t;
-    while(K--) {
+
+    v.push_back({10});
+    v.push_back({1});
+    v.push_back({6, 2, 4, 8});
+    v.push_back({1, 3, 9, 7});
+    v.push_back({6, 4});
+    v.push_back({5});
+    v.push_back({6});
+    v.push_back({1, 7, 9, 3});
+    v.push_back({6, 8, 4, 2});
+    v.push_back({1, 9});
+
+    while (K--) {
         cin >> a >> b;
-        t = 1;
-
-        while(b--) {
-            t = (t * a) % 10;
-        }
-
-        if (!t)
-            t = 10;
-        cout << t << '\n';
+        a %= 10;
+        b %= v[a].size();
+        cout << v[a][b] << '\n';
     }
     
     return 0;
