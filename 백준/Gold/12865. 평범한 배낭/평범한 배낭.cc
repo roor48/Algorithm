e01d7a9e83e4f75090cbc782ced05eb44ec6d0ac
roor48 <109197337+roor48@users.noreply.github.com>
2025-09-19 13:45:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,30 +3,22 @@ using namespace std;
 
 int N, K;
 int W[100], V[100];
-int dp[100][100001];
-
-int f(int n, int k) {
-    if (k>K)
-        return -1000000;
-    if (n==N)
-        return 0;
-
-    if (dp[n][k] != -1)
-        return dp[n][k];
-
-    return dp[n][k] =  max(f(n+1, k+W[n])+V[n], f(n+1, k));
-
-}
+int dp[100001];
 
 int main() {
-    memset(dp, -1, sizeof dp);
 
     cin >> N >> K;
     for (int i = 0; i < N; i++) {
         cin >> W[i] >> V[i];
     }
 
-    cout << f(0, 0);
-    
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k <= K-W[i]; k++) {
+            dp[k] = max(dp[k], dp[k+W[i]] + V[i]);
+        }
+    }
+
+    cout << dp[0];
+   
     return 0;
 }
