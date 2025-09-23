9c9dcef0dfdeb408f665398f1a7194e9a53f7379
roor48 <109197337+roor48@users.noreply.github.com>
2025-09-23 14:37:33 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct {
-    int cost;
-    int client;
-}city;
-
 int C, N;
-city arr[1001];
 int dp[1100];
 int main() {
-    fill(dp, dp+1100, 1'000'000);
+    fill(dp+1, dp+1100, 1'000'000);
 
     cin >> C >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i].cost >> arr[i].client;
-    }
 
     int ans = 1'000'000;
-    dp[0] = 0;
-    for (int i = 0; i < N; i ++) {
-        for (int c = arr[i].client; c <= 1099; c++) {
-            dp[c] = min(dp[c], dp[c-arr[i].client] + arr[i].cost);
+    int cost, client;
+    for (int i = 0; i < N; i++) {
+        cin >> cost >> client;
 
-            if (c>=C) {
-                ans = min(ans, dp[c]);
+        for (int c = client; c <= 1099; c++) {
+            dp[c] = min(dp[c], dp[c-client] + cost);
+    
+            if (c>=C && ans>dp[c]) {
+                ans = dp[c];
             }
         }
     }
